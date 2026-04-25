4e6dc2ba44c67bf0e4d443f05caf9a84298da492
Alberto Sierra <alberto.sierra@murciaeduca.es>
2026-04-25 18:32:22 +0200

    Mail sin verificación TLS

--- resources/views/emails/header.blade.php
+++ resources/views/emails/header.blade.php
@@ -1,5 +1,5 @@
 <div style="text-align: center; margin-bottom: 20px;">
-    <img src="{{ asset('/ediciones/edicion' . \App\Models\Edicion::getEdicionActual()->id . '/banner.png') }}" alt="Olimpiadas Informáticas" style="width: 100%; max-width: 600px; height: auto; border-radius: 10px;">
+    <img src="{{ asset('/storage/ediciones/edicion' . \App\Models\Edicion::getEdicionActual()->id . '/banner.png') }}" alt="Olimpiadas Informáticas" style="width: 100%; max-width: 600px; height: auto; border-radius: 10px;">
     <br>
     <h1 style="color: #364f59;">Olimpiadas Informáticas</h1>
 </div>
